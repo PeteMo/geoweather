dac5d4e186c0dff7761b02ca1d2a5847536e4db8
Pete Morris <pete.morris@gmail.com>
2011-05-09 13:15:35 -0400

    Cache the external IP.

--- geoweather.py
+++ geoweather.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-import sys, urllib2, os.path
+import sys, urllib2, os, datetime
 import pygeoip
 
 cache_dir = os.path.expanduser('~') + '/.geoweather'
@@ -9,10 +9,28 @@ cache_dir = os.path.expanduser('~') + '/.geoweather'
 def getExternalIP():
     src = 'http://www.whatismyip.com/automation/n09230945.asp'
     ip_cache = cache_dir + '/ip.txt'
+    cache_timeout = datetime.timedelta(seconds=300)
 
-    # Use cached data if we checked recently, otherwise hit src.
+    # Get the last modified time from the cache file if it exists; otherwise, 
+    # fake the mtime to ensure the cache is created.
+    try:
+        stat = os.stat(ip_cache)
+        mtime = datetime.datetime.fromtimestamp(stat.st_mtime)
+    except OSError:
+        mtime = datetime.datetime(1980, 01, 01)
 
-    return urllib2.urlopen(src).read()
+    # Use the cache if it is valid.
+    if datetime.datetime.today() < mtime + cache_timeout:
+        print "Using cached IP"
+        f = open(ip_cache, 'r')
+        ip = f.readline()
+    else:
+        ip = urllib2.urlopen(src).read()
+        f = open(ip_cache, 'w')
+        f.write("%s\n" % ip)
+
+    f.close()
+    return ip
 
 
 def getLoc():
